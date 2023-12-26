b0c086ed7dbc51e8375b3eebde04a46e4687fedb
ReStranger <strengerplayr@gmail.com>
2023-12-26 22:58:12 +0300

    Adding two designs and a blank to the third

---
+++ bspwm/rices/montagnes_violettes/change_to_montagnes_violettes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+nitrogen --set-zoom-fill $HOME/Pictures/Wallpapers/Firefox_wallpaper.png --save --head=0
+cp -rf $HOME/.config/rices/montagnes_violettes/.gtkrc-2.0 $HOME/.gtkrc-2.0
+cp -rf $HOME/.config/rices/montagnes_violettes/.config/* $HOME/.config/
+kitty +kitten themes --reload-in=all "Montagnes Violettes"
+pkill picom
+pkill dunst 
+pkill conky
+bspc wm -r
+sleep 2
+dunstify "Theme: Montagnes Violettes successfully applied!"
