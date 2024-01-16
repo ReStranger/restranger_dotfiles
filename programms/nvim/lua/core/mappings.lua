407b3e79e39ea06f685a54ce1e03371dd83c7ba1
ReStranger <strengerplayr@gmail.com>
2024-01-16 23:55:51 +0300

    fix tmux config and updated nvim config

--- programms/nvim/lua/core/mappings.lua
+++ programms/nvim/lua/core/mappings.lua
@@ -463,23 +463,4 @@ M.gitsigns = {
   },
 }
 
-M.dap = {
-  plugin = true,
-  n = {
-    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
-  }
-}
-
-M.dap_python = {
-  plugin = true,
-  n = {
-    ["<leader>dpr"] = {
-      function()
-        require('dap-python').test_method()
-      end
-    }
-  }
-}
-
-
 return M
--- programms/nvim/lua/custom/configs/lspconfig.lua
+++ programms/nvim/lua/custom/configs/lspconfig.lua
@@ -1,7 +1,6 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
-
-local lspconfig = require "lspconfig"
+local lspconfig = require ("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd" }
@@ -28,5 +27,6 @@ vim.api.nvim_create_autocmd('FileType', {
     })
   end,
 })
+
 -- 
 -- lspconfig.pyright.setup { blabla}
