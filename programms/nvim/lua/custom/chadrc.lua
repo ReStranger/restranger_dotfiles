3cf2fc011965d4fbe69f85c266b174e34cfe1408
ReStranger <strengerplayr@gmail.com>
2024-02-22 21:20:42 +0300

    Added vscode support to nvim

--- programms/nvim/lua/custom/chadrc.lua
+++ programms/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "montagnes_violettes",
-  theme_toggle = { "montagnes_violettes", "one_light" },
+  theme = "yoru",
+  theme_toggle = { "yoru", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
