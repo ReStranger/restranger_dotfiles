3e87655485b5078e580d0e34ba1cb873e94436e4
ReStranger <strengerplayr@gmail.com>
2024-06-19 15:54:04 +0300

    updated nvim config

--- programms/nvim/lua/configs/statuscol.lua
+++ programms/nvim/lua/configs/statuscol.lua
@@ -5,12 +5,12 @@ statuscol.setup {
   segments = {
     { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
     {
-      sign = { namespace = { "Diagnostic" }, maxwidth = 2, auto = true },
+      sign = { namespace = { "Diagnostic" }, maxwidth = 1, auto = true },
       click = "v:lua.ScSa",
     },
     { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
     {
-      sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, wrap = true },
+      sign = { name = { ".*" }, maxwidth = 1, colwidth = 1, wrap = true },
       click = "v:lua.ScSa",
     },
   },
