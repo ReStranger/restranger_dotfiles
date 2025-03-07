48ba49dc23f49b31d945da0fabb6f55f9c7cc4b9
ReStranger <restranger@disroot.org>
2025-03-08 02:51:58 +0300

    feet(firefox): Add user.js setup
    Signed-off-by: ReStranger <restranger@disroot.org>

---
+++ firefox/user.js
@@ -0,0 +1,39 @@
+// Scrolling
+user_pref("general.smoothscroll.lines.durationMaxms", 125);
+user_pref("general.smoothscroll.lines.durationMinms", 125);
+user_pref("general.smoothscroll.mousewheel.durationMaxms", 200);
+user_pref("general.smoothscroll.mousewheel.durationMinms", 100);
+user_pref("general.smoothscroll.msdphysics.enabled", true);
+user_pref("general.smoothscroll.ether.durationMaxms", 125);
+user_pref("general.smoothscroll.other.durationminms", 125);
+user_pref("general.smoothscroll.pages.durationMaxms", 125);
+user_pref("general.smoothscroll.pages.durationminms", 125);
+
+user_pref("mousewheel.min_line_scroll_amount", 30);
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
+user_pref(
+  "mousewheel.system_scroll_override_on_root_content.horizontal.factor",
+  175,
+);
+user_pref(
+  "mousewheel.system_scroll_override_on_root_content.vertical.factor",
+  175,
+);
+user_pref("toolkit.scrollbox.horizontalscrolldistance", 6);
+user_pref("toolkit.scrollbox.verticalscrolldistance", 2);
+
+// Perf
+user_pref("gfx.webrender.all", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("media.hardware-video-decoding.enabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("browser.newtabpage.activity-stream.feeds.system.topstories", false);
+
+// Privacy
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("media.peerconnection.ice.default_address_only", true);
+user_pref("network.captive-portal-service.enabled ", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("network.dns.echconfig.enabled", true);
+user_pref("network.dns.http3_echconfig.enabled", true);
